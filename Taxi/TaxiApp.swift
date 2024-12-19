4bbce8ae9ea2644dbf105805fb6aac13ffc6e161
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-12-19 14:36:24 +0500

    Initial Commit

---
+++ Taxi/TaxiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxiApp.swift
+//  Taxi
+//
+//  Created by Dmitry Yastrebov on 19.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct TaxiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
